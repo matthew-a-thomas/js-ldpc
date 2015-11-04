832ba3b6c71a28fc67dc46d69e33fa6bd8e34e79
Matthew Thomas <me@matthewathomas.com>
2015-11-03 21:28:14 -0500

    Added base-n functionality & extra examples

--- ldpc.js
+++ ldpc.js
@@ -14,19 +14,28 @@ function LDPC(options) {
 	// See https://en.wikipedia.org/wiki/Low-density_parity-check_code
 	//  for explanations of p, -p(transposed), the generator matrix, and
 	//  the parity matrix
-	var p = LDPC.util.make(options.k, options.n - options.k, function() {
-		return Math.floor(random.next() * options.modulo);
+	var invertibleNumbers = [];
+	var i = -1;
+	for (var i = 1; i < options.modulo; i++) {
+		if (LDPC.util.multiplicativeInverse(i, options.modulo))
+			invertibleNumbers.push(i);
+	}
+	var p = LDPC.util.make(options.k, options.n - options.k, function(row, column) {
+		if ((row % options.k) == (column % options.k) && column > 0)
+			return 0;
+		else
+			return random.nextFromChoice(invertibleNumbers);
 	});
-	var negPT = LDPC.util.fix(LDPC.util.map(LDPC.util.transpose(p), function(value) {
-		return -1 * value;
-	}), options.modulo);
+	var negPT = LDPC.util.map(LDPC.util.transpose(p), function(value) {
+		return LDPC.util.mod(-value, options.modulo);
+	});
 
 	var generator = LDPC.util.concatColumns(LDPC.util.identity(options.k), p);
 	var parity = LDPC.util.concatColumns(negPT, LDPC.util.identity(options.n - options.k));
 
 	// Wikipedia says that we can check that the row space of G is
 	//  orthogonal to H by doing this:
-	var test = LDPC.util.fix(LDPC.util.multiply(generator, LDPC.util.transpose(parity)), options.modulo);
+	var test = LDPC.util.multiply(generator, LDPC.util.transpose(parity), options.modulo);
 	// Every element of test should be zero
 	LDPC.util.map(test, function(value) {
 		if (value)
@@ -34,65 +43,101 @@ function LDPC(options) {
 	});
 
 	/**
-	 * Attempts to decode the given encoded message
+	 * Attempts to decode the given encoded message. Returns an array of
+	 * options.k symbols. The returned array has a "decoded" property that tells
+	 * if the encoded message was successfully decoded completely (the same as
+	 * there being no erased symbols in the returned result)
 	 */
 	this.decode = function(encoded) {
-		encoded = encoded.slice();
-		var resolvedAtLeastOne = true;
-		while (resolvedAtLeastOne) {
-			resolvedAtLeastOne = false;
+		var encoded = LDPC.util.deepCopy(encoded);
+		var matrix = [];
+		for (var i = 0; i < parity.length; i++) {
+			matrix.push([]);
+		}
+		var sums = [];
+		var variables = [];
 
-			// See if any of the parity constraints can be satisfied
-			parity.forEach(function(constraints) {
-				var missingSymbolIndices = [];
-				var total = 0;
-				for (var i = 0; i < constraints.length; i++) {
-					var constraint = constraints[i];
-					var symbol = encoded[i];
-					if (constraint > 0) {
-						if (symbol < 0 || symbol == null) {
-							// This symbol has been erased / is missing
-							missingSymbolIndices.push(i);
-						}
-						else {
-							// This symbol hasn't been erased
-							total += symbol * constraint;
-						}
+		for (var i = 0; i < encoded.length; i++) {
+			var symbol = encoded[i];
+			if (symbol == null || symbol < 0) {
+				// This symbol is missing. Therefore we need to consider it
+				//  as a variable to solve for
+				variables.push(i);
+				if (variables.length > parity.length)
+					break; // No point in continuing because we won't be able to solve this system of equations completely
+				for (var j = 0; j < parity.length; j++) {
+					var parityRow = parity[j];
+					matrix[j].push(parityRow[i]);
+				}
+			} else {
+				// This symbol is present, so it should contribute toward
+				//  the sum we're trying to solve for
+				for (var j = 0; j < parity.length; j++) {
+					var parityRow = parity[j];
+					sums[j] = sums[j] || 0;
+					sums[j] -= symbol * parityRow[i];
+					//sums[j] = LDPC.util.mod(sums[j], options.modulo);
+				}
+			}
+		}
+
+		if (variables.length <= parity.length) { // Only continue if we've got a change of solving the system of equations
+			for (var j = 0; j < parity.length; j++) {
+				matrix[j].push(sums[j]); // The sums will fill the right-hand column
+			}
+
+			var reduced = LDPC.util.gaussReduction(matrix, options.modulo); // This tries to put the matrix into reduced row-echelon form
+
+			for (var i = 0; i < reduced.length; i++) {
+				var reducedRow = reduced[i];
+				// If there's exactly one "1" and the rest "0" (except for the
+				//  last element), then the last element is the answer for the
+				//  variable indicated by the field with the "1" in it
+				var variableIndex = -1;
+				for (var j = 0; j < reducedRow.length - 1; j++) {
+					var element = reducedRow[j];
+					if (element == 1 && variableIndex == -1) {
+						// We haven't yet found a "1" element
+						variableIndex = j;
+					} else if (element) {
+						// This row doesn't tell us anything conclusively
+						variableIndex = -1;
+						break;
 					}
 				}
-				if (missingSymbolIndices.length == 1) {
-					var missingSymbolIndex = missingSymbolIndices.pop();
-					var constraint = constraints[missingSymbolIndex];
-					// We can figure out this one missing symbol. Its value
-					//  will make the total add up to zero mod
-					//  options.modulo
-					var missingValue = (((-total * constraint) % options.modulo) + options.modulo) % options.modulo;
-					encoded[missingSymbolIndex] = missingValue;
-					resolvedAtLeastOne = true;
-				} else if (missingSymbolIndices.length == 0) {
-					// There are no missing symbols, so let's check that
-					//  that this parity constraint holds true
-					if (total % options.modulo)
-						throw "Total isn't zero in mod options.modulo";
+				if (variableIndex != -1) {
+					// We found an answer: the variableIndex'th variable equals the last element in this row
+					var answer = reducedRow[reducedRow.length - 1];
+					//console.log("variable " + variableIndex + "=" + answer);
+					encoded[variables[variableIndex]] = answer;
 				}
-			});
+			}
 		}
-		var retVal = encoded.slice(0, options.k); // Return the first k symbols
-		// Determine if the result is completely decoded yet; .decoded will indicate that
+		var retVal = encoded.slice(0, options.k);
 		retVal.decoded = true;
 		retVal.forEach(function(value) {
-			retVal.decoded &= value > 0 || value === 0;
-		});
-		retVal.result = encoded; // .result will be the full array of symbols
+			if (value == null || value < 0) {
+				retVal.decoded = false;
+			}
+		})
+		retVal.all = encoded;
+
 		return retVal;
-	};
+	}
 
 	/**
 	 * Encodes the given message, returning an array options.n symbols
 	 * long
 	 */
 	this.encode = function(message) {
-		return LDPC.util.fix(LDPC.util.multiply([message], generator), options.modulo)[0];
+		return LDPC.util.multiply([message], generator, options.modulo)[0];
+	};
+
+	/**
+	 * Returns a copy of the generator matrix used by this instance of LDPC
+	 */
+	this.getGenerator = function() {
+		return LDPC.util.deepCopy(generator);
 	};
 
 	/**
@@ -120,6 +165,13 @@ LDPC.Random = function(seed) {
 	};
 
 	/**
+	 * Picks a random choice from the given choices
+	 */
+	this.nextFromChoice = function(choices) {
+		return choices[Math.floor(this.next() * choices.length)];
+	};
+
+	/**
 	 * Generates a normal number (mean of zero) with given standard
 	 * deviation
 	 */
@@ -142,6 +194,23 @@ LDPC.Random = function(seed) {
 			p *= this.next();
 		} while (p > L);
 		return k - 1;
+	};
+
+	/**
+	 * Scrambles the list of numbers from 0 to (length-1)
+	 */
+	this.nextSet = function(length) {
+		var set = [];
+		for (var i = 0; i < length; i++) {
+			set.push(i);
+		}
+		for (var i = 0; i < set.length; i++) {
+			var randomIndex = Math.floor(this.next() * set.length);
+			var temp = set[i];
+			set[i] = set[randomIndex];
+			set[randomIndex] = temp;
+		}
+		return set;
 	};
 };
 
@@ -169,7 +238,7 @@ LDPC.util.concatColumns = function(array1, array2) {
 LDPC.util.deepCopy = function(matrix) {
 	var copy = [];
 	matrix.forEach(function(value, index, object) {
-		if (typeof value == "object") {
+		if (value != null && typeof value == "object") {
 			copy.push(LDPC.util.deepCopy(value));
 		} else {
 			copy.push(value);
@@ -186,6 +255,86 @@ LDPC.util.fix = function(array, modulo) {
 	});
 };
 
+// Performs Gauss reduction for a finite field, putting it into row-echelon
+//  form.
+LDPC.util.gaussReduction = function(matrix, modulo) {
+	matrix = LDPC.util.fix(matrix, modulo); // Note this makes a copy of the given matrix, and also ensures that all elements are < modulo
+	if (!matrix[0])
+		return matrix;
+	var numColumns = matrix[0].length;
+	var numRows = matrix.length;
+	var reducedRows = {};
+	var scale = function(row, multiple) {
+		for (var col = 0; col < row.length; col++) {
+			row[col] *= multiple;
+			row[col] = LDPC.util.mod(row[col], modulo);
+		}
+	};
+	var subtractRow = function(from, row, multiple) {
+		//console.log("subtracting " + multiple + " multiples of " + row.toString() + " from " + from.toString());
+		for (var col = 0; col < from.length; col++) {
+			from[col] -= multiple * row[col];
+			from[col] = LDPC.util.mod(from[col], modulo);
+		}
+		//console.log("...got " + from.toString());
+	};
+	for (var col = 0; col < numColumns && col < numRows; col++) {
+		//console.warn("working column " + col);
+		// Get zeros in this column for all rows but one, which will become a
+		//  reduced row
+		var numZeros;
+		var lastMinimumRow = -1;
+		do {
+			var minimumRow = -1;
+			numZeros = 0;
+			// Find the row with the minimum value for this column
+			for (var row = 0; row < numRows; row++) {
+				if (!reducedRows[row]) { // We don't want to scale a row that has already been reduced, nor do we want to consider it the minimum row
+					// Perhaps we can reduce this row to 1 in this column by using a multiplicative inverse
+					var multiplicativeInverse = LDPC.util.multiplicativeInverse(matrix[row][col], modulo);
+					if (multiplicativeInverse) {
+						scale(matrix[row], multiplicativeInverse);
+						minimumRow = row;
+						//console.log("scaled row " + row + " " + multiplicativeInverse + " times to " + matrix[row].toString() + ", which made it the minimum row");
+						break; // Skips the rest of this for-loop
+					}
+					if ((minimumRow == -1 || matrix[row][col] < matrix[minimumRow][col]) && matrix[row][col] != 0) {
+						// Otherwise, see if this row should be considered the minimum row
+						minimumRow = row;
+					}
+				}
+			}
+			//console.log("minimum row is " + minimumRow);
+			if (minimumRow == lastMinimumRow) {
+				// We're going around in circles. Time to stop
+				//console.log("we're being circular because we've already decided that " + minimumRow + " is the minimum row");
+				break;
+			} else {
+				lastMinimumRow = minimumRow;
+			}
+			// Subtract multiples of that row from all the other rows
+			for (var row = 0; row < numRows; row++) {
+				if (row != minimumRow && matrix[row][col]) {
+					subtractRow(matrix[row], matrix[minimumRow], Math.floor(matrix[row][col] / matrix[minimumRow][col]));
+				}
+				if (matrix[row][col] == 0) {
+					//console.log("row " + row + " has a zero");
+					numZeros++;
+				}
+			}
+			//console.log(matrix.toString());
+		} while (numZeros < numRows - 1);
+		for (var row = 0; row < numRows; row++) {
+			if (matrix[row][col]) {
+				//console.log("row " + row + " is reduced");
+				reducedRows[row] = true;
+				break;
+			}
+		}
+	}
+	return matrix;
+};
+
 // Generates a random array of numbers between 0 and the given modulo
 LDPC.util.getRandomSeries = function(length, modulo) {
 	var retVal = [];
@@ -200,6 +349,23 @@ LDPC.util.identity = function(dimension) {
 	return LDPC.util.make(dimension, dimension, function(row, column) {
 		return row == column ? 1 : 0;
 	});
+};
+
+// Finds the inverse of a 2x2 matrix in the given modulo
+LDPC.util.inverse2x2 = function(matrix, modulo) {
+	var a = matrix[0][0];
+	var b = matrix[0][1];
+	var c = matrix[1][0];
+	var d = matrix[1][1];
+	var determinantInv = LDPC.util.multiplicativeInverse(
+		a * d - b * c,
+		modulo);
+	return LDPC.util.fix(
+		[
+			[determinantInv * d, determinantInv * -b],
+			[determinantInv * -c, determinantInv * a]
+		],
+		modulo);
 };
 
 // Returns a two-dimensional array with the given number of rows and
@@ -246,15 +412,50 @@ LDPC.util.map = function(array, calculate) {
 	return copy;
 };
 
-// http://stackoverflow.com/questions/27205018/multiply-2-matrices-in-javascript
-LDPC.util.multiply = function(matrix1, matrix2) {
-    var result = [];
+// Takes care of negative numbers as well
+LDPC.util.mod = function(x, modulo) {
+	return ((x % modulo) + modulo) % modulo;
+};
+
+// Lazily-computes (and then caches) the multiplicative inverse of a number
+LDPC.util.multiplicativeInverse = function() {
+	var cache = {};
+	return function(x, modulo) {
+		x = LDPC.util.mod(x, modulo);
+		var inverses;
+		if (!(inverses = cache[modulo] || []).length) {
+			for (var i = 1; i < modulo; i++) {
+				if (inverses[i] != null){
+					continue;
+				} else {
+					for (var multiplier = 1; multiplier < modulo; multiplier++) {
+						if (LDPC.util.mod(i * multiplier, modulo) == 1) {
+							// i and multiplier are multiplicative inverses of each other in mod modulo
+							inverses[i] = multiplier;
+							inverses[multiplier] = i;
+							break;
+						}
+					}
+				}
+			}
+			cache[modulo] = inverses;
+		}
+		return inverses[x];
+	};
+}();
+
+// Multiplies two matrices in the given modulo
+LDPC.util.multiply = function(matrix1, matrix2, modulo) {
+    var result = [[]];
+    if (!matrix2.length)
+    	return result;
     for (var i = 0; i < matrix1.length; i++) {
         result[i] = [];
         for (var j = 0; j < matrix2[0].length; j++) {
             var sum = 0;
             for (var k = 0; k < matrix1[0].length; k++) {
                 sum += matrix1[i][k] * matrix2[k][j];
+                sum %= modulo;
             }
             result[i][j] = sum;
         }
@@ -264,9 +465,11 @@ LDPC.util.multiply = function(matrix1, matrix2) {
 
 // Returns a transposed copy of the given array
 LDPC.util.transpose = function(array) {
-	var rows = array.length;
-	var columns = array[0].length;
 	var transposed = [];
+	var rows = array.length;
+	if (!rows)
+		return transposed;
+	var columns = array[0].length;
 	for (var i = 0; i < columns; i++) {
 		var row = [];
 		for (var j = 0; j < rows; j++) {
