6084e52c217c6c9c3e4ab1af16caa4872618f0dd
Jake Prather <JakeHP@Zoho.com>
2016-01-09 23:30:13 -0600

    Spec boilerplate.

---
+++ test/randomGeneratorSpec.js
@@ -0,0 +1,8 @@
+var chai = require('chai');
+var assert = chai.assert;
+var expect = chai.expect;
+chai.should();
+
+describe('randomGenerator', function () {
+     
+});
---
+++ test/utilitySpec.js
@@ -0,0 +1,119 @@
+var chai = require('chai');
+var assert = chai.assert;
+var expect = chai.expect;
+chai.should();
+
+describe('utility', function () {
+    describe('#concatColumns()', function () {
+        it('should do x', function () {
+
+        });
+        it('should do y', function () {
+
+        });
+    });
+    describe('#deepCopy()', function () {
+        it('should do x', function () {
+
+        });
+        it('should do y', function () {
+
+        });
+    });
+    describe('#fix()', function () {
+        it('should do x', function () {
+
+        });
+        it('should do y', function () {
+
+        });
+    });
+    describe('#gaussReduction()', function () {
+        it('should do x', function () {
+
+        });
+        it('should do y', function () {
+
+        });
+    });
+    describe('#getRandomSeries()', function () {
+        it('should do x', function () {
+
+        });
+        it('should do y', function () {
+
+        });
+    });
+    describe('#identity()', function () {
+        it('should do x', function () {
+
+        });
+        it('should do y', function () {
+
+        });
+    });
+    describe('#inverse2x2()', function () {
+        it('should do x', function () {
+
+        });
+        it('should do y', function () {
+
+        });
+    });
+    describe('#make()', function () {
+        it('should do x', function () {
+
+        });
+        it('should do y', function () {
+
+        });
+    });
+    describe('#map()', function () {
+        it('should do x', function () {
+
+        });
+        it('should do y', function () {
+
+        });
+    });
+    describe('#mod()', function () {
+        it('should do x', function () {
+
+        });
+        it('should do y', function () {
+
+        });
+    });
+    describe('#multiplicativeInverse()', function () {
+        it('should do x', function () {
+
+        });
+        it('should do y', function () {
+
+        });
+    });
+    describe('#clearCache()', function () {
+        it('should do x', function () {
+
+        });
+        it('should do y', function () {
+
+        });
+    });
+    describe('#multiply()', function () {
+        it('should do x', function () {
+
+        });
+        it('should do y', function () {
+
+        });
+    });
+    describe('#transpose()', function () {
+        it('should do x', function () {
+
+        });
+        it('should do y', function () {
+
+        });
+    });
+});
