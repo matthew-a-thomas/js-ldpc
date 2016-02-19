70b1f294918b6fc6808c55d274cb4e053f38bc01
Jake Prather <JakeHP@Zoho.com>
2016-02-18 21:36:41 -0600

    Change ldpc construction. Update docs api. Update project deps.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var jscs = require('gulp-jscs');
 
 gulp.task('lint', () => {
-    return gulp.src('src/**/*.js')
-        .pipe(jscs())
-        .pipe(jscs.reporter());
+    return gulp.src('src/*.js')
+    .pipe(jscs())
+    .pipe(jscs.reporter());
 });
